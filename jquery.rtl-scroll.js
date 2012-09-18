27a3e4ee627490042c4d025d0a9fc381ec7f18a2
othree_kao <othree_kao@htc.com>
2012-09-18 11:07:36 +0800

    Init commit

---
+++ jquery.rtl-scroll.js
@@ -0,0 +1,19 @@
+/*MIT License */
+/*global jQuery */
+(function ($) {
+    'use strict';
+    var definer = $('<div dir="rtl" style="font-size: 14px; width: 1px; height: 1px; position: absolute; top: -1000px; overflow: scroll">A<div>').appendTo('body')[0],
+        type = 'reverse';
+
+    if (definer.scrollLeft > 0) {
+        type = 'default';
+    } else {
+        definer.scrollLeft = 1;
+        if (definer.scrollLeft === 0) {
+            type = 'negative';
+        }
+    }
+
+    $(definer).remove();
+    $.support.rtlScrollType = type;
+}(jQuery));
