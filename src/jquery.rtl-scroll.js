8eb3a98a7ee2ee535e6bc677405f3b27ee102a1d
othree <othree@gmail.com>
2020-02-18 23:32:19 +0800

    Two space indent

--- src/jquery.rtl-scroll.js
+++ src/jquery.rtl-scroll.js
@@ -1,28 +1,28 @@
 /*MIT License */
 /*global jQuery */
 jQuery(function() {
-    'use strict';
+  'use strict';
 
-    var definer = $('<div dir="rtl" style="width: 1px; height: 1px; position: fixed; top: 0px; left: 0px; overflow: hidden"><div style="width: 2px"><div style="display: inline-block; width: 1px"></div><div style="display: inline-block; width: 1px"></div></div></div>').appendTo('body')[0],
-        type = 'reverse';
+  var definer = $('<div dir="rtl" style="width: 1px; height: 1px; position: fixed; top: 0px; left: 0px; overflow: hidden"><div style="width: 2px"><div style="display: inline-block; width: 1px"></div><div style="display: inline-block; width: 1px"></div></div></div>').appendTo('body')[0],
+      type = 'reverse';
 
-    if (definer.scrollLeft > 0) {
-        type = 'default';
+  if (definer.scrollLeft > 0) {
+    type = 'default';
+  } else {
+    if (Element.prototype.scrollIntoView) {
+      definer.children[0].children[1].scrollIntoView();
+      if (definer.scrollLeft < 0) {
+        type = 'negative';
+      }
     } else {
-        if (Element.prototype.scrollIntoView) {
-            definer.children[0].children[1].scrollIntoView();
-            if (definer.scrollLeft < 0) {
-                type = 'negative';
-            }
-        } else {
-            definer.scrollLeft = 1;
-            if (definer.scrollLeft === 0) {
-                type = 'negative';
-            }
-        }
+      definer.scrollLeft = 1;
+      if (definer.scrollLeft === 0) {
+        type = 'negative';
+      }
     }
+  }
 
-    $(definer).remove();
-    $.support.rtlScrollType = type;
+  $(definer).remove();
+  $.support.rtlScrollType = type;
 });
 
